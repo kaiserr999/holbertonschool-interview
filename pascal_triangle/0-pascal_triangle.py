2cbf5124030dddb432f4c28509708c977a8ff291
Kaiserr999 <I.camelin74@gmail.com>
2026-05-21 10:42:01 +0200

    Fix PEP8 line too long error

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -6,7 +6,8 @@ Module for Pascal's Triangle
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal's triangle of n.
+    Returns a list of lists of integers representing the Pascal's
+    triangle of n.
     """
     if n <= 0:
         return []
